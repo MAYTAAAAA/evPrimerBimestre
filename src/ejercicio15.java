6b6c5b8138252eed0b6e17d92a9cace677f38336
MAYTAAAAA <fiufiuale2@gmail.com>
2025-04-10 10:21:15 -0300

    evaluacionPrimeroAlemayta

---
+++ src/ejercicio15.java
@@ -0,0 +1,27 @@
+package ejercicio15;
+
+import java.util.Scanner;
+
+public class ejercicio15 {
+public static void main(String[] args) {
+	Scanner hola= new Scanner(System.in);
+    char numero;
+    
+    System.out.println("ingrese una letra para la talla S,L y M");
+    numero=hola.next().charAt(0);
+    
+   switch (numero) {
+   case 's' :
+	   System.out.println("te quedan 5 remeras cutrutul");
+	   break
+   case 'm':
+	  System.out.println("te quedan 2 remeras cuturul");
+	  break;
+   case 'l':
+    System.out.println("no te quedan remeras");
+    break;
+    
+   }
+    
+}
+}
